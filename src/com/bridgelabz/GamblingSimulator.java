f66fc7c819e883941c06eaa8f7438d3ff22f12aa
“santhoshi123654” <santhoshi.badam@gmail.com>
2021-11-22 13:05:36 +0530

    Added: The gambler with a Stake and Bet

---
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class GamblingSimulator {
+	
+	private static final int Stake = 100;
+	private static final int Bet = 1;
+	
+
+	public static void main(String[] args) {
+		System.out.println("Gambler starting with a stake of : $" + Stake + " and bet in every game is : $" + Bet);
+	}
+	
+}
